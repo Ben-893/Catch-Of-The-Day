7ec167a431d6ba1e973a019c95b57af55c0d704c
Ben-893 <ben.smith.gordon@gmail.com>
2021-03-24 16:10:19 +0000

    Created methods for removing fish from the Inventory, as well as from the Order

--- src/components/App.js
+++ src/components/App.js
@@ -56,12 +56,29 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  deleteFish = key => {
+    // 1. Take a copy of state
+    const fishes = { ...this.state.fishes };
+    // 2. Delete the fish
+    fishes[key] = null;
+    // 3. Update the State
+    this.setState({ fishes });
+  };
+
   addToOrder = key => {
     // 1. Take a copy of the existing State
     const order = { ...this.state.order };
     // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
+  removeFromOrder = key => {
+    const order = { ...this.state.order };
+
+    delete order[key];
+
     this.setState({ order });
   };
 
@@ -85,10 +102,15 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
         />
