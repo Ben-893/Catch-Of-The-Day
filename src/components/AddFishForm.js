b516e4bb292516d8313acf2edd213173c602108a
Ben-893 <ben.smith.gordon@gmail.com>
2021-04-05 22:06:40 +0100

    Added authentication to the app

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddFishForm extends React.Component {
+  
   nameRef = React.createRef();
   priceRef = React.createRef();
   statusRef = React.createRef();
